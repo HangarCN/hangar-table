eaa15297889be7782952f38005f18d7360b3c45d
mysmiles <1490767581@qq.com>
2022-04-11 09:16:58 +0800

    修改jsx支持

--- packages/core/components_use.js
+++ packages/core/components_use.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import { Input, Button, Select, Table } from 'ant-design-vue'
+import { Input, Button, Select, Table, Empty } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
 
 import HTable from '../HTable/index.vue';
 
@@ -9,6 +10,7 @@ export default {
 }
 
 Vue.use(Input);
+Vue.use(Empty);
 Vue.use(Button);
 Vue.use(Select);
 Vue.use(Table);
--- vite.config.js
+++ vite.config.js
@@ -21,14 +21,21 @@ export default {
     sourcemap: true, // 输出.map文件
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: ['vue', 'ant-design-vue'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          vue: 'Vue'
+          vue: 'Vue',
+          'ant-design-vue': 'ant-design-vue'
         }
       }
-    }
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
   plugins: [createVuePlugin({ jsx: true })],
   css: {
